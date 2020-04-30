084822532b8cf575b83dbc6eb644a2a392981a06
Juani Balaguer <balaguerjuani@gmail.com>
2020-04-30 18:03:46 -0300

    Sorting Visualizer

--- src/SortingVisualizer/SortingVisualizer.jsx
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -10,7 +10,7 @@ import animationHandler from './AnimationHandlers';
 const COLOR = "white";
 const SECOND_COLOR = "deeppink";
 const FINAL_COLOR = 'rgb(0, 221, 129)';
-let running = false;
+let isDesabled = false;
 
 class SortingVisualizer extends React.Component {
     
@@ -80,7 +80,6 @@ class SortingVisualizer extends React.Component {
 
     bubbleSort() {
         this.disableButtons();
-        console.log(running.toString())
         const animations = getAnimationsBubble(this.state.array);
         const bars = document.getElementsByClassName("array-bar");
         for (let i = 1; i < animations.length; i++) {
@@ -133,18 +132,33 @@ class SortingVisualizer extends React.Component {
         document.getElementById("newArray").disabled = true;
         document.getElementById("speedbar").disabled = true;
         document.getElementById("arraybar").disabled = true;
-        
+        document.getElementById("mergeSort").className = "disabled";
+        document.getElementById("insertionSort").className = "disabled";
+        document.getElementById("quickSort").className = "disabled";
+        document.getElementById("heapSort").className = "disabled";
+        document.getElementById("bubbleSort").className = "disabled";
+        document.getElementById("newArray").className = "disabled";
+        document.getElementById("speedbar").className = "disabledSlider";
+        document.getElementById("arraybar").className = "disabledSlider";
     }
 
     enableButtons() {
         document.getElementById("mergeSort").disabled = false
         document.getElementById("insertionSort").disabled = false;
-        document.getElementById("quickSort").disabled = false
-        document.getElementById("heapSort").disabled = false
-        document.getElementById("bubbleSort").disabled = false
-        document.getElementById("newArray").disabled = false
+        document.getElementById("quickSort").disabled = false;
+        document.getElementById("heapSort").disabled = false;
+        document.getElementById("bubbleSort").disabled = false;
+        document.getElementById("newArray").disabled = false;
         document.getElementById("speedbar").disabled = false;
         document.getElementById("arraybar").disabled = false;
+        document.getElementById("mergeSort").className = "algobutton";
+        document.getElementById("insertionSort").className = "algobutton";
+        document.getElementById("quickSort").className = "algobutton";
+        document.getElementById("heapSort").className = "algobutton";
+        document.getElementById("bubbleSort").className = "algobutton";
+        document.getElementById("newArray").className = "algobutton";
+        document.getElementById("arraybar").className = "slider";
+        document.getElementById("speedbar").className = "slider";
     }
 
     randomBetween(max, min) {
@@ -154,7 +168,7 @@ class SortingVisualizer extends React.Component {
     render() {
         let array = this.state.array;
         return (
-        <div>
+        <div id="main-container">
             <div className="bar-container">
              {array.map((value, id) => (
                     <div
@@ -171,20 +185,20 @@ class SortingVisualizer extends React.Component {
             </div> 
             <div id="toolbar">
                 <div id="sliders">
-                    <div id="slider"> <p>Change array size</p>
-                    <input class="slider" id="arraybar" disabled={running ? "true" : null} type="range" min="20" max="300" value={this.state.array.length} onChange={this.sliderHandler} />
-                </div>
-                <div id="slider"> <p>Change animation speed</p>
-                    <input class="slider" id="speedbar" disabled={running ? "true" : null} type="range" min="1" max="300" value={this.state.ANIM_SPEED} onChange={this.speedHandler} />
-                </div>
+                    <div class="slidercontainer"> <p>Array size</p>
+                        <input class="slider" id="arraybar" type="range" min="20" max="300" value={this.state.array.length} onChange={this.sliderHandler} />
+                    </div>
+                    <div class="slidercontainer"> <p>Animation speed</p>
+                        <input class="slider" id="speedbar" type="range" min="1" max="300" value={this.state.ANIM_SPEED} onChange={this.speedHandler} />
+                    </div>
                 </div>
                 <div id="buttons">
-                    <button class="algobutton" id="newArray" disabled={running ? "true" : null} onClick={() => this.resetArray(document.getElementById("arraybar").value)} >New array</button>
-                    <button class="algobutton" id="mergeSort" disabled={running ? "true" : null} onClick={() => this.mergeSort()} >Merge Sort</button>
-                    <button class="algobutton" id="insertionSort" disabled={running ? "true" : null} onClick={() => this.insertionSort()} >Insertion Sort</button>
-                    <button class="algobutton" id="bubbleSort" disabled={running ? "true" : null} onClick={() => this.bubbleSort()} >Bubble Sort</button>
-                    <button class="algobutton" id="quickSort" disabled={running ? "true" : null} onClick={() => this.quickSort()} >Quick Sort</button>
-                    <button class="algobutton" id="heapSort" disabled={running ? "true" : null} onClick={() => this.heapSort()} >Heap Sort</button>
+                    <button class="algobutton" id="newArray" onClick={() => this.resetArray(document.getElementById("arraybar").value)} >New array</button>
+                    <button class="algobutton" id="mergeSort"  onClick={() => this.mergeSort()} >Merge Sort</button>
+                    <button class="algobutton" id="insertionSort"  onClick={() => this.insertionSort()} >Insertion Sort</button>
+                    <button class="algobutton" id="bubbleSort"  onClick={() => this.bubbleSort()} >Bubble Sort</button>
+                    <button class="algobutton" id="quickSort"  onClick={() => this.quickSort()} >Quick Sort</button>
+                    <button class="algobutton" id="heapSort"  onClick={() => this.heapSort()} >Heap Sort</button>
                 </div>
             </div>
         </div>
